b14813295cfd6824eebdaec44217509d83b7d7ea
Damilare <damilare.durojaye@gmail.com>
2017-08-16 20:14:07 +0100

    Updated Ionic version. I hope this does not cause a breaking change

---
+++ src/app/app.firebase.config.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG = {
+    apiKey: "GET YOUR OWN VERSION",
+    authDomain: "GET YOUR OWN VERSION",
+    databaseURL: "GET YOUR OWN VERSION",
+    projectId: "GET YOUR OWN VERSION",
+    storageBucket: "GET YOUR OWN VERSION",
+    messagingSenderId: "GET YOUR OWN VERSION"
+};
